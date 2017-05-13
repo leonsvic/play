73bc389ddb6d4ed87d5f4d5e54cda818499c1f87
leonsvic <leonsvic@gmail.com>
2017-05-13 13:03:54 +0000

    1st commit, this version works, issue is that last range result cannot be printed

---
+++ gettaskdist.sh
@@ -0,0 +1,38 @@
+#/bin/bash
+
+taskinfofile=$1
+
+echo "hello"
+
+range=(0 10 20 30 40 50 60 70 80 90 100 110 120 300 600 1800 3600 7200 14400 144000)
+for (( i=0; i<${#range[@]}; i++ ))
+do
+	[ ${range[$i]} -eq 0 ] && continue
+	j=$((i-1))
+	echo ${range[$j]}" - "${range[$i]}
+
+	rm -rf tmptask
+	cat $taskinfofile  | awk -F "," '{print($4)}' | sort -g | uniq -c | sed '1d' | while read line
+	do
+		tasktime=`echo $line | cut -d" " -f2`
+		[ $tasktime -ge ${range[$j]} ] && [ $tasktime -lt ${range[$i]} ]  && echo $line >> tmptask
+	done
+
+	[ -f tmptask ] && python getsum.py tmptask
+	echo "----"
+#	python getsum.py tmptask
+
+done
+rm -rf tmptask
+
+
+
+# original simple testing version
+exit
+
+cat $taskinfofile  | awk -F "," '{print($4)}' | sort -g | uniq -c | while read line
+do 
+	[ `echo $line | cut -d" " -f2` -le 10 ] && echo $line >> tmptask
+done
+
+python getsum.py tmptask
