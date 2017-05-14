5135c8f581ae9decd530d859c1709b76fe236677
leonsvic <leonsvic@gmail.com>
2017-05-14 12:01:24 +0000

    all done: (1) with percentage (2) could handle both mr and sql taskinfo

--- gettaskdist.sh
+++ gettaskdist.sh
@@ -7,7 +7,7 @@ for (( i=0; i<${#range[@]}; i++ ))
 do
 	[ ${range[$i]} -eq 0 ] && continue
 	j=$((i-1))
-	echo -n ${range[$j]}" - "${range[$i]}", "
+	echo -n ${range[$j]}" "${range[$i]}" " | tee -a tmpresult
 
 	rm -rf tmptask
 	cat $taskinfofile  | awk -F "," '{print($4)}' | sort -g | uniq -c | sed '1d' | while read line
@@ -18,17 +18,36 @@ do
 
 	if [ -f tmptask ] 
 	then
-		# if just one line in tmptask, getsum doesn't work, so print the only line out directly
-		[ `cat tmptask | wc -l` -eq "1" ] && cat tmptask | awk '{print($1)}'  && continue 
-		#python getsum.py tmptask  && continue
-		python getsum.py tmptask | awk '{print($2)}' && continue
+		# if just one line in tmptask, getsum.py doesn't work, so print the only line out directly
+		[ `cat tmptask | wc -l` -eq "1" ] && cat tmptask | awk '{print($1)}' | tee -a  tmpresult && continue 
+		python getsum.py tmptask | awk '{print($2)}' | sed -e "s/\.$//g" |  tee -a tmpresult && continue
 	fi
 	#if goes here, means there's no sampling in current range, put a 0 here 
-	echo "0"
+	echo "0" | tee -a tmpresult
 #	echo "----"
 done
 
+# put a sum line
+echo "--"
+#cat tmpresult
+echo -n "SUM:"
+sum=`python getsum.py tmpresult | sed -e "s/\s//g" | cut -d. -f3`
+echo $sum
+echo "--"
+
+# calculate the percentage of each line
+percentage_accumulated=0
+cat tmpresult | while read line
+do
+	echo -n $line | sed -e "s/ /, /g"
+	nbr=`echo $line | cut -d" " -f3`
+	percentage=$(echo "scale=4;$nbr/$sum"|bc)
+	percentage_accumulated=$(echo "scale=4;$percentage_accumulated+$percentage"|bc)
+	#echo " "$percentage" "$percentage_accumulated
+	printf ", %.2f%%, %.2f%%\n" $(echo "$percentage*100"|bc) $(echo "$percentage_accumulated*100"|bc)
+done
 
 rm -rf tmptask
+rm -rf tmpresult
 
 # add a line to see how git diff works
